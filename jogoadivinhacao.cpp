b3f395395282bcd710a3855bad91f88a2d62165c
anne-pinho <anne.pinhov@gmail.com>
2024-08-29 15:27:59 -0400

    add score in guessing game

--- jogoadivinhacao.cpp
+++ jogoadivinhacao.cpp
@@ -11,6 +11,8 @@ int main (){
     bool nao_acertou = true;
     int tentativas = 0;
 
+    double pontos = 1000.0;
+
     while(nao_acertou){
         tentativas++;
         int chute;
@@ -19,15 +21,16 @@ int main (){
         cout << "Qual o seu chute? ";
         cin >> chute;
 
-        cout << "o valor do seu chute é: " << chute << endl;
+        double pontos_perdidos = abs(chute - NUMERO_SECRETO)/2.0;
+        pontos = pontos - pontos_perdidos;
 
+        cout << "o valor do seu chute é: " << chute << endl;
         bool acertou = chute == NUMERO_SECRETO;
         bool maior = chute > NUMERO_SECRETO;
 
         if (acertou){
             cout << "Parabéns, você acertou o numero secreto!" << endl;
             nao_acertou = false;
-            cout << "Voce acertou em " << tentativas << " tentativas" << endl;
         } else if (maior){
             cout << "Seu chute foi maior que o numero secreto" << endl;
         } else {
@@ -36,4 +39,8 @@ int main (){
     }
     
     cout << "Fim de jogo!" << endl;
+    cout << "Voce acertou em " << tentativas << " tentativas" << endl;
+    cout.precision(2);
+    cout << fixed;
+    cout << "Sua pontuação foi de " << pontos << " pontos" << endl;
 }
