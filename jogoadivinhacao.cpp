08287c5d030cead95de0b3937cbfddbd20c8808b
anne-pinho <anne.pinhov@gmail.com>
2024-08-29 11:59:41 -0400

    add jogo da adivinhacao

---
+++ jogoadivinhacao.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "*****************************************" << endl;
+    cout << "Bem vindos ao Jogo da Adivinhação" << endl;
+    cout << "*****************************************" << endl;
+
+    int numero_secreto = 42;
+
+    cout << "o numero secreto é " << numero_secreto << endl;
+}
