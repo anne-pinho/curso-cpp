96123a0870d87d8e4cd11052477e2fd73cac55da
anne-pinho <anne.pinhov@gmail.com>
2024-08-29 15:02:46 -0400

    add improvements in play

--- jogoadivinhacao.cpp
+++ jogoadivinhacao.cpp
@@ -6,7 +6,34 @@ int main (){
     cout << "Bem vindos ao Jogo da Adivinhação" << endl;
     cout << "*****************************************" << endl;
 
-    int numero_secreto = 42;
+    const int NUMERO_SECRETO = 42;
+    
+    bool nao_acertou = true;
+    int tentativas = 0;
 
-    cout << "o numero secreto é " << numero_secreto << endl;
+    while(nao_acertou){
+        tentativas++;
+        int chute;
+
+        cout << "Tentativa " << tentativas << endl;
+        cout << "Qual o seu chute? ";
+        cin >> chute;
+
+        cout << "o valor do seu chute é: " << chute << endl;
+
+        bool acertou = chute == NUMERO_SECRETO;
+        bool maior = chute > NUMERO_SECRETO;
+
+        if (acertou){
+            cout << "Parabéns, você acertou o numero secreto!" << endl;
+            nao_acertou = false;
+            cout << "Voce acertou em " << tentativas << " tentativas" << endl;
+        } else if (maior){
+            cout << "Seu chute foi maior que o numero secreto" << endl;
+        } else {
+            cout << "Seu chute foi menor que o numero secreto" << endl;
+        }      
+    }
+    
+    cout << "Fim de jogo!" << endl;
 }
