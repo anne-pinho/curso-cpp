4c5f71910fa34cd1fdb9bb0cc4d464da48df3f74
anne-pinho <anne.pinhov@gmail.com>
2024-09-01 14:52:30 -0400

    add show kicks

--- forca.cpp
+++ forca.cpp
@@ -1,33 +1,73 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA";
+map<char, bool> chutou;
+vector<char> chutes_errados;
 
-bool letra_existe(char chute){
-    for(char letra : PALAVRA_SECRETA){
-        if (chute == letra){
+bool letra_existe(char chute)
+{
+
+    for (char letra : PALAVRA_SECRETA)
+    {
+        if (chute == letra)
+        {
             return true;
         }
     }
     return false;
 }
 
-int main(){
-    
-    cout << PALAVRA_SECRETA << endl;
-
+int main()
+{
+    cout << "****************************" << endl;
+    cout << "Jogo da Forca" << endl;
+    cout << "****************************" << endl;
+    cout << endl;
     bool nao_acertou = true;
     bool nao_enforcou = true;
 
-    while (nao_acertou && nao_enforcou){
-       char chute;
-       cin >> chute;
-
-       if(letra_existe(chute)){
-            cout << "Você acertou! Seu chute está na palavra." <<endl; 
-        } else {
-            cout << "Você errou! Seu chute não está na palavra." <<endl; 
+    while (nao_acertou && nao_enforcou)
+    {
+        cout << "Chutes errados: ";
+        for (char letra: chutes_errados){
+            cout << letra << " ";
         }
+        cout << endl;
+
+
+        for (char letra : PALAVRA_SECRETA)
+        {
+            if (chutou[letra])
+            {
+                cout << letra << "";
+            }
+            else
+            {
+                cout << "_ ";
+            }
+        }
+
+        cout << endl;
+        cout << "Seu chute: ";
+        char chute;
+        cin >> chute;
+
+        chutou[chute] = true;
+
+        if (letra_existe(chute))
+        {
+            cout << "Você acertou! Seu chute está na palavra." << endl;
+        }
+        else
+        {
+            cout << "Você errou! Seu chute não está na palavra." << endl;
+            chutes_errados.push_back(chute);
+        }
+
+        cout << endl;
     }
 }
